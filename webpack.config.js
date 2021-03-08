8a098e38bf331cc998d54562322cbca58881a486
ToqaMoh <toqa9713@hotmail.com>
2021-03-08 15:53:31 +0200

    Second Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,31 @@
 const path = require("path") 
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: '/node_modules/',
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                exclude: '/node_modules/',
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
